7e28c06cfe7b7a0aef5196fef9e7aec649b4bcc7
HazemEltawel <eltawelhazem5@gmail.com>
2025-07-21 16:13:28 +0300

    Question 02

--- Experince-5-OOP/Program.cs
+++ Experince-5-OOP/Program.cs
@@ -1,14 +1,58 @@
-﻿using Assignment_5_OPP.Question_01;
-using System.Drawing;
+﻿using Assignment_5_OPP.Question_02;
+using Assignment_5_OPP.Question_01;
 using System;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 class Program
 {
+    private static string password;
+
     static void Main(string[] args)
     {
-        Assignment_5_OPP.Question_01.MyCircle circle = new MyCircle(5);
+        #region Question 01
+        //Assignment_5_OPP.Question_01.MyCircle circle = new MyCircle(5);
 
-        Assignment_5_OPP.Question_01.IRectangle rectangle = new My_Rectangle(4, 6);
-        circle.DisplayShapeInfo();
-        rectangle.DisplayShapeInfo();  
-     }
-}
+        //Assignment_5_OPP.Question_01.IRectangle rectangle = new My_Rectangle(4, 6);
+        //circle.DisplayShapeInfo();
+        //rectangle.DisplayShapeInfo();
+        #endregion
+
+        #region Question 02
+
+        IAuthenticationService authService = new BasicAuthenticationService();
+
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        if (authService.Authenticate(username, password))
+        {
+            Console.Write("Enter role to check: ");
+            string role = Console.ReadLine();
+
+            if (authService.Authenticate(username, role))
+            {
+                Console.WriteLine("User is authenticated and authorized.");
+            }
+            else
+            {
+                Console.WriteLine("User is authenticated but not authorized.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Authentication failed.");
+        }
+
+        #endregion
+
+
+
+
+
+
+
+    }
+    }
